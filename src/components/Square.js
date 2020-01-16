6ace08535209f1b80a93dd3df4511501925ade8f
kim jae hun <zg788990@gmail.com>
2020-01-16 15:55:09 +0900

    tic-tac-toe

---
+++ src/components/Square.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Square(props) {
+  const className = 'square' + (props.highlight ? ' highlight' : '');
+  return (
+    <button className={className} onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
+}
+
+export default Square;
